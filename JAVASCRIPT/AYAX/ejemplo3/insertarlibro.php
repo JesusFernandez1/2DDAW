27a41656ab9e9648762bd794d806cd254799c0c3
Jesus <jesus20102000@gmail.com>
2022-11-23 14:12:27 +0100

    commit

---
+++ JAVASCRIPT/AYAX/ejemplo3/insertarlibro.php
@@ -0,0 +1,24 @@
+<?php
+
+try {
+    include('conex.php');
+    $conex=Conecta();
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $editorial = $_POST['editorial'];
+    $paginas = $_POST['paginas'];
+    $anno = $_POST['anno'];
+
+    $query = mysqli_query(
+        $conex, "INSERT INTO 'tareas' ('Titulo', 'Autor', 'Editora', 'Paginas', 'Anio') VALUES
+        ($titulo, $autor, $editorial, $paginas, $anno");
+
+        echo $query;
+
+}
+catch(\Exception $e) {
+    echo $e->getMessage();
+}
+finally {
+    mysqli_close($conex); //$conex=$mysqly
+}
--- JAVASCRIPT/AYAX/ejemplo3/utiles.js
+++ JAVASCRIPT/AYAX/ejemplo3/utiles.js
@@ -1,28 +1,4 @@
 $(document).ready(function () {
-  $("#contenido").on("click", ".borrar", function () {
-    var id = $(this).parent().siblings().eq(0).html();
-    console.log(id);
-  });
-
-  $.ajax({
-    url: "borrarlibro.php=" + id,
-    type: "GET",
-    dataType: "text",
-    success: function (datos) {
-      $(this).parent().parent().delete();
-    },
-
-    error: function (xhr, status) {
-      alert("Disculpe, existió un problema");
-    },
-    complete: function (xhr, status) {
-      //alert('Petición realizada');
-    },
-  });
-});
-
-$(document).ready(function () {
-
   $.ajax({
     url: "damelibro.php",
     type: "GET",
@@ -31,11 +7,11 @@ $(document).ready(function () {
       datos = datos.substring(0, datos.length - 1);
       var datos2 = datos.split(",");
       var libros =
-        '<table border=1 class="table table-stripped"><tr><th>idlibro</td><td>Titulo</td><td>Autor</td><td>Editora</td><td>Paginas</td><td>Anios</td><td>Acciones</td></tr>';
+        '<table border=1 class="table table-stripped"><tr><th>idlibro</td><th>Titulo</th><th>Autor</th><th>Editora</th><th>Paginas</th><th>Anios</th><th>Acciones</th></tr>';
       $.each(datos2, function (idx, elem) {
-        if (idx % 6 == 0) {
+        if (idx % 6 == 0 && idx != 0) {
           libros =
-            libros + '<td><button class="Borrar">Borrar</button></td></tr><tr>';
+            libros + '<td><button class="borrar">Borrar</button></td></tr><tr>';
         }
 
         libros = libros + "<td>" + elem + "</td>";
@@ -50,5 +26,50 @@ $(document).ready(function () {
     complete: function (xhr, status) {
       //alert('Petición realizada');
     },
+  });
+});
+
+$(document).ready(function () {
+  $("#contenido").on("click", ".borrar", function () {
+    var id = $(this).parent().siblings().eq(0).html();
+    console.log(id);
+    var fila = $(this).parent().parent();
+  });
+
+  $.ajax({
+    url: "borrarlibro.php?id=" + id,
+    type: "GET",
+    dataType: "text",
+    success: function (datos) {
+      fila.remove();
+    },
+
+    error: function (xhr, status) {
+      alert("Disculpe, existió un problema");
+    },
+    complete: function (xhr, status) {
+      //alert('Petición realizada');
+    },
+  });
+  $("#insertar").click(function () {
+    $.ajax({
+      url: "insertarlibro.php",
+      type: "POST",
+      dataType: "text",
+      data: {
+        titulo: $("#titulo").val(),
+        autor: $("#autor").val(),
+        editorial: $("#editorial").val(),
+        paginas: $("#paginas").val(),
+        anno: $("#anno").val(),
+      },
+      success: function (datos) {},
+      error: function (xhr, status) {
+        alert("Disculpe, existió un problema");
+      },
+      complete: function (xhr, status) {
+        //alert('Petición realizada');
+      },
+    });
   });
 });
