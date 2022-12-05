254db5d477dd84bf4e28d8c0c5c92f60fb07c786
Jesus <71879707+Braeek@users.noreply.github.com>
2022-12-05 01:28:06 +0100

    Delete CursoPHP directory

--- CursoPHP/Bloque4/ejercicio28.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ($_GET) {
-    include('ejercicio27.php');
-} else {
-    include('ejercicio27.html');
-}
-
-?>
--- CursoPHP/Ejemplo 2/conex_bbdd.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conex=mysqli_connect('localhost', 'root', '', 'provincias');
-
-if (! $conex) {
-	die("<p>ERROR EN la conexión</p>");
-}
